7e1b6a24fd139b1663c8cf4393d3637f6abb7527
Mike Rissen <mrissen@centriq.com>
2022-05-20 10:12:17 -0500

    Initial commit

---
+++ EFCoreMappings/EFCoreMappings/Controllers/HomeController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EFCoreMappings.Models;
+
+namespace EFCoreMappings.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly TestCtx _db;
+
+        public HomeController(TestCtx context)
+        {
+            _db = context;
+        }
+
+        // GET: Home
+        public async Task<IActionResult> Index()
+        {
+              return View(await _db
+                  .Companies
+                  //.Include("CompanyType")
+                  .ToListAsync());
+        }
+
+        // GET: Home/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _db.Companies == null)
+            {
+                return NotFound();
+            }
+
+            var company = await _db.Companies
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return View(company);
+        }
+
+        // GET: Home/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name")] Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(company);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(company);
+        }
+
+        // GET: Home/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _db.Companies == null)
+            {
+                return NotFound();
+            }
+
+            var company = await _db.Companies.FindAsync(id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+            return View(company);
+        }
+
+        // POST: Home/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name")] Company company)
+        {
+            if (id != company.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _db.Update(company);
+                    await _db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CompanyExists(company.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(company);
+        }
+
+        // GET: Home/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _db.Companies == null)
+            {
+                return NotFound();
+            }
+
+            var company = await _db.Companies
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return View(company);
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_db.Companies == null)
+            {
+                return Problem("Entity set 'TestCtx.Companies'  is null.");
+            }
+            var company = await _db.Companies.FindAsync(id);
+            if (company != null)
+            {
+                _db.Companies.Remove(company);
+            }
+            
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CompanyExists(int id)
+        {
+          return _db.Companies.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ EFCoreMappings/EFCoreMappings/Models/Company.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCoreMappings.Models
+{
+    public class Company
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        [ForeignKey("CompanyType")]
+        public int CompanyTypeId { get; set; }
+        public CompanyType CompanyType { get; set; }
+    }
+    public class CompanyType
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public string Abbreviation { get; set; }
+    }
+    public class TestCtx : DbContext
+    {
+        public TestCtx(DbContextOptions<TestCtx> opts) : base(opts) {   }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //  Uncomment me or Controller Include
+            //modelBuilder
+            //    .Entity<Company>()
+            //    .Navigation(d => d.CompanyType)
+            //    .AutoInclude();
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<CompanyType> CompanyTypes { get; set; }
+
+        public void BuildTestData()
+        {
+            CompanyTypes.AddRange(new List<CompanyType>()
+            {
+                new CompanyType() { Description = "Food Service/Restaurant", Abbreviation = "Food"},
+                new CompanyType() { Description = "Public Works", Abbreviation = "BWorks"},
+                new CompanyType() { Description = "Banking", Abbreviation = "Bank"},
+                new CompanyType() { Description = "Law Enforcement", Abbreviation = "PoPo"},
+                new CompanyType() { Description = "IT Consulting", Abbreviation = "ITCons"},
+                new CompanyType() { Description = "Security", Abbreviation = "Sec"},
+            });
+            SaveChanges();
+            Companies.Add(new Company()
+            {
+                Name = "Bubba Gump Shrimp Company",
+                CompanyType = CompanyTypes.First(d => d.Abbreviation == "Food")
+            });
+            SaveChanges();
+        }
+    }
+}
